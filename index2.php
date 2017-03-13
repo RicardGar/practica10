2d66a144e1a78abaea4f4679b3cfb4132e51d562
Ricardo Garcia Guillen <richardroquegg@gmail.com>
2017-03-13 17:33:57 -0600

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,18 @@
+/* comentario*/
+//comentario en toda la linea
+#comentario
+<html>
+<head>
+    <tittle>práctica 10</tittle>
+</head>
+    <body>
+        <h1>usando PHP</h1>
+        <p><?php
+        echo "parrafo PHP";
+            ?></p>
+        <p>usando html</p>
+        <h3><?php
+         echo "parrafo php";
+            ?></h3>
+    </body>
+</html>
